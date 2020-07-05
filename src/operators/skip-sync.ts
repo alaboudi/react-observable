66c8a25873b0604d0a910ddb8e1e065e7dbdf549
Yazan Alaboudi <yazan.alaboudi@gmail.com>
2020-07-04 21:11:08 -0400

    add jsDoc for skipSync operator

--- src/operators/skip-sync.ts
+++ src/operators/skip-sync.ts
@@ -1,9 +1,25 @@
 import { share } from "rxjs/operators";
 import { Observable } from "rxjs";
 
-const noop = () => {}
-
+/**
+ * This operator skips all synchronously emitted values from an observable.
+ *
+ * @example
+ * const source$ = new BehaviorSubject<number>(1);
+ * const noSyncSource$ = source$.pipe(skipSync);
+ *
+ * source$.subscribe(console.log);
+ * noSyncSource$.subscribe(console.log)
+ * source$.next(2);
+ *
+ * // source$ observer will log 1 & 2
+ * // noSycSource$ will only log 2
+ *
+ * @param {Observable<T>} source$
+ * @return {Observable<T>}
+ */
 const skipSync = <T>(source$: Observable<T>): Observable<T> => {
+    const noop = () => {}
     const shared$ = source$.pipe(share());
     const subscription = shared$.subscribe(noop);
     subscription.unsubscribe();
