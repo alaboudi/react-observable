d26e33229368b655d108525eefcd24590e0a9737
Yazan Alaboudi <yazan.alaboudi@gmail.com>
2020-07-21 18:08:48 -0400

    change implementation details of use-observable

--- src/use-observable.ts
+++ src/use-observable.ts
@@ -1,24 +1,31 @@
-import {Observable, OperatorFunction} from "rxjs";
+import {MonoTypeOperatorFunction, Observable } from "rxjs";
 import {useEffect, useState} from "react";
 import memoize from "./utility/memoize";
 import getLastSyncEmission from "./utility/get-last-sync-emission";
-import pubRefCountEnhancer from "./utility/enhance-pub-refcount";
+import survivableShare from "./operators/survivable-share";
 
-const resolveInitialValue = <T>(source$: Observable<T>, enhancer: OperatorFunction<T, T>, initialValue?: T): (T | undefined) => {
-    const enhancedSource$ = enhancer(source$);
+const resolveInitialValue = <T>(source$: Observable<T>, operatorFunction: MonoTypeOperatorFunction<T>, initialValue?: T): (T | undefined) => {
+    const enhancedSource$ = operatorFunction(source$);
     const lastSyncEmission = getLastSyncEmission(enhancedSource$);
     return lastSyncEmission === undefined ? initialValue : lastSyncEmission;
 }
 
 const useObservable = <T>(source$: Observable<T>, initialValue?: T): T => {
-    const [enhancer] = useState(() => memoize(pubRefCountEnhancer))
-    const [emittedValue, setEmittedValue] = useState<T>(() => resolveInitialValue(source$, enhancer, initialValue));
+    const [operatorFunction] = useState(() => memoize(
+        survivableShare(1) as MonoTypeOperatorFunction<T>)
+    );
+
+    const [emittedValue, setEmittedValue] = useState<T>(() => resolveInitialValue(
+        source$,
+        operatorFunction,
+        initialValue
+    ));
 
     useEffect(() => {
-        const enhancedSource$ = enhancer(source$);
+        const enhancedSource$ = operatorFunction(source$);
         const subscription = enhancedSource$.subscribe(setEmittedValue);
         return () => subscription.unsubscribe();
-    }, [source$, setEmittedValue, enhancer]);
+    }, [source$, setEmittedValue, operatorFunction]);
 
     return emittedValue;
 };
